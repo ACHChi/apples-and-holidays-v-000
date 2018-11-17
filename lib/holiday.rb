6e85fa61c49a5c97f54209535bdf02be8cad51b9
Chao Hsuan Chi <allen0413@gmail.com>
2018-11-17 06:23:16 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect{|season, holiday|
     holiday.collect{|holiday, item|
       if item == "BBQ"
-        holiday
+        retuen holiday
       end
     }.flatten
   }
